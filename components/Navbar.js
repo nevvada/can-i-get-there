d6553d7a2cf3b53eb8c9926e4aa30b5567bdef7b
nlemast <nlemaster47@gmail.com>
2019-08-06 23:15:26 -0400

    Created the layout, navbar, about, index page with Next.js & React.js

---
+++ components/Navbar.js
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+
+const Navbar = () => (
+  <>
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/about">About</Link>
+      </li>
+    </ul>
+
+    <style jsx>{`
+      ul {
+        background: #333;
+        color: #fff;
+        list-style: none;
+        display: flex;
+      }
+
+      ul li {
+        font-size: 18px;
+        margin-right: 20px;
+      }
+
+      ul li a {
+        color: #fff;
+        text-decoration: none;
+      }
+    `}</style>
+  </>
+);
+
+export default Navbar;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+const Index = () => (
+  <Layout>
+    <>
+      <h1>Welcome to Can I Get There</h1>
+    </>
+  </Layout>
+);
+
+export default Index;
