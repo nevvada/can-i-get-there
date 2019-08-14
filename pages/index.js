195d80d8c94e7c7365949d37400f63a29f87a0bc
nlemast <nlemaster47@gmail.com>
2019-08-14 00:06:18 -0400

    converted functional component to class; created getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,36 @@
+import React, { Component } from 'react';
 import Fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
 import Form from '../components/Form';
 
-const Index = props => (
-  <Layout>
-    <>
-      <h1>Welcome to Can I Get There</h1>
-      <Form />
-      {/* {props} */}
-    </>
-  </Layout>
-);
+export default class Index extends Component {
+  render() {
+    return (
+      <Layout>
+        <>
+          <h1>Welcome to Can I Get There</h1>
+          <Form />
+          {/* {props} */}
+        </>
+      </Layout>
+    );
+  }
+}
 
-// Index.getInitialProps = async function() {
-//   const res = await fetch(
-//     'http://web.mta.info/developers/data/nyct/nyct_ene.xml'
-//   );
-//   const data = await res.text();
-//   console.log(data);
-//   return { data };
-// };
+// const Index = props => (
+//   <Layout>
+//     <>
+//       <h1>Welcome to Can I Get There</h1>
+//       <Form />
+//       {/* {props} */}
+//     </>
+//   </Layout>
+// );
 
-export default Index;
+Index.getInitialProps = async function() {
+  const res = await fetch(
+    'http://web.mta.info/developers/data/nyct/nyct_ene.xml'
+  );
+  console.log(res);
+  return { res };
+};
