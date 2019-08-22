55c2984a76c55d9240c85b03d5a002841b245c74
nlemast <nlemaster47@gmail.com>
2019-08-21 21:51:51 -0400

    deleted unused file because business logic is in a diff component

--- components/GetData.js
+++
@@ -1,9 +0,0 @@
-const GetData = () => {
-  fetch('http://web.mta.info/developers/data/nyct/nyct_ene.xml')
-    .then(res => res.text())
-    .then(data => {
-      console.log('biiiitch', data);
-    });
-};
-
-export default GetData;
