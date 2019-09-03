16d95ed5a22c4d48fdbb46f04f9632263668a0b5
nlemast <nlemaster47@gmail.com>
2019-09-02 22:19:14 -0400

    updated form and option card

--- components/Form.js
+++ components/Form.js
@@ -3,70 +3,99 @@ import ReactDOM from 'react-dom';
 import 'isomorphic-unfetch';
 import OptionCard from './OptionCard';
 import axios from 'axios';
-// import GetData from './GetData';
-// import retrieveElevatorData from '../data/retrieveElevatorData';
 
 export default class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
       query: '',
+      subwayStations: null,
       matches: [],
       isClicked: false,
-      stationClicked: null,
-      subwayClicked: null
+      stationClicked: null
     };
 
     this.changeSelected = this.changeSelected.bind(this);
   }
 
-  changeSelected(id) {
+  clearInput = () => {
+    document.querySelector('.station-input').value = '';
+  };
+
+  // select specific subway station from list of suggested
+  changeSelected(station) {
     this.setState({
-      stationClicked: this.state.matches[id].station,
-      subwayClicked: this.state.matches[id].subways,
+      query: '',
+      stationClicked: station,
       matches: []
     });
-    console.log('aaaa', this.state);
-    this.props.showElevatorStatus(this.state.matches[id].station);
+    this.props.showElevatorStatus(station);
+    // this.props.showElevatorStatus(this.state.matches[id].station);
   }
 
-  onChange = async e => {
+  // autosuggest up to 5 suggestions based on what user has typed
+  onChange = e => {
     this.state.query = e.target.value;
-    const res = await fetch('https://api.myjson.com/bins/1buo5h');
-    const subways = await res.json();
 
-    let matches = subways.filter(subway => {
-      const regex = new RegExp(`^${this.state.query}`, 'gi');
+    // if input is empty, don't show any matches
+    if (this.state.query === '') {
+      this.setState({ matches: [] });
+      return;
+    }
+
+    // match input text to possible subway stations
+    let matches = this.state.subwayStations.filter(subway => {
+      const regex = new RegExp(`.*?${this.state.query}`, 'gi');
       return subway.station.match(regex);
     });
 
+    // only show 5 options. set as state
     matches = matches.slice(0, 5);
     this.setState({ matches: matches });
   };
 
+  // fetch list of subway stations
+  componentDidMount() {
+    axios
+      .get('https://api.myjson.com/bins/o8yhz')
+      .then(res => {
+        this.setState({
+          subwayStations: res.data
+        });
+      })
+      .catch(err => console.error(err));
+  }
+
   render() {
     const matches = this.state.matches;
-    let isClicked = this.state.isClicked;
     return (
-      <>
+      <div className="form-styles">
         <form>
           <input
+            className="station-input"
             type="text"
             name="station"
             onChange={this.onChange.bind(this)}
+            autoComplete="off"
           />
+          <span className="blinking-cursor">|</span>
+          <span className="directions">
+            Just type a station to get started!
+          </span>
         </form>
         {matches.map((each, i) => {
           return (
             <OptionCard
-              id={i}
+              key={i}
               station={each.station}
+              locations={each.locations}
               subways={each.subways}
-              onClick={this.changeSelected}
+              changeSelected={this.changeSelected}
+              clearInput={this.clearInput}
             />
           );
         })}
-      </>
+      </div>
     );
   }
 }
--- components/OptionCard.js
+++ components/OptionCard.js
@@ -1,10 +1,63 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+const addStyling = subwayLine => {
+  if (subwayLine.innerHTML === 'A') {
+    subwayLine.addAt;
+  }
+};
 
 const OptionCard = props => {
-  // console.log('baby', props);
   return (
-    <div onClick={() => props.onClick(props.id)}>
-      {props.station} - {props.subways}
+    <div
+      className="option-card"
+      onClick={() => {
+        props.changeSelected(props.station);
+        props.clearInput();
+      }}
+    >
+      <section className="subway-station">{props.station}</section>
+      <section className="subway-lines">
+        {props.subways.map(each => {
+          let color;
+          if (each === 'A' || each === 'C' || each === 'E') {
+            color = 'blue';
+          } else if (
+            each === 'B' ||
+            each === 'D' ||
+            each === 'F' ||
+            each === 'M'
+          ) {
+            color = 'orange';
+          } else if (each === 'G') {
+            color = 'lightgreen';
+          } else if (each === 'J' || each === 'Z') {
+            color = 'brown';
+          } else if (each === 'L') {
+            color = 'lightgrey';
+          } else if (
+            each === 'N' ||
+            each === 'Q' ||
+            each === 'R' ||
+            each === 'W'
+          ) {
+            color = 'yellow';
+          } else if (each === 'S') {
+            color = 'darkgrey';
+          } else if (each === '1' || each === '2' || each === '3') {
+            color = 'red';
+          } else if (each === '4' || each === '5' || each === '6') {
+            color = 'green';
+          } else if (each === '7') {
+            color = 'purple';
+          }
+
+          return (
+            <span key={each} className={`subway-line ${color}-line`}>
+              {each}
+            </span>
+          );
+        })}
+      </section>
     </div>
   );
 };
