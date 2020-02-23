a45632b603b3972619c89d52edbf4670011427ed
nlemast <nlemaster47@gmail.com>
2020-02-23 16:50:16 -0500

    Build out methods to findStation from API, renderSuggestions

--- src/StationFormContainer.tsx
+++ src/StationFormContainer.tsx
@@ -11,11 +11,42 @@ class StationFormContainer extends Component {
     selectedStation: '',
   };
 
+  findStation = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { target: { value } } = event;
+
+    if (value.length === 0) {
+      this.setState({ searchedStations: [] });
+      return;
+    }
+
+    const regex = new RegExp(value, 'gi');
+    const matches = stationsAPI.filter(station => station.stationName.match(regex));
+    const matchesToDisplay = matches.slice(0, 5);
+
+    this.setState({ searchedStations: matchesToDisplay });
+  }
+
+  setSelectedStationAndFetchElevatorStatus = (station) => {
+    this.setState({ selectedStation });
+  }
+
+  renderSuggestions = () => {
+    const { searchedStations } = this.state;
+
+    return searchedStations.length 
+      ? searchedStations.map(station => <FormSuggestion stationInfo={station} />)
+      : <div />
+  }
+
   render() {
     return (
       <>
         <h2>This is the station form container.</h2>
+        <StationForm findStation={this.findStation} />
+        {this.renderSuggestions()}
       </>
     )
   }
 }
+
+export default StationFormContainer
