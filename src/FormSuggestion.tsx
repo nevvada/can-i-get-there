32c7b290bb43e6bed7f357060ea18f489f6bd0a6
nlemast <nlemaster47@gmail.com>
2020-02-24 21:56:13 -0500

    Import SubwayLogo component and create subwayDict

--- src/FormSuggestion.tsx
+++ src/FormSuggestion.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
 import { Station } from './typings/subwayStation';
+import SubwayLogo from './SubwayLogo';
 
 interface Props {
   stationInfo: Station;
+}
+
+const subwayDict = {
+  A: 'blue',
+  C: 'blue',
+  E: 'blue',
+  B: 'orange',
+  D: 'orange',
+  F: 'orange',
+  M: 'orange',
+  G: 'light-green',
+  J: 'brown',
+  Z: 'brown',
+  L: 'light-gray',
+  N: 'yellow',
+  Q: 'yellow',
+  R: 'yellow',
+  S: 'dark-gray',
+  '1': 'red',
+  '2': 'red',
+  '3': 'red',
+  '4': 'green',
+  '5': 'green',
+  '6': 'green',
+  '7': 'purple'
 }
 
 const renderSubwayLineLogos = (subwayLines: Station['subwayLines']) => {
