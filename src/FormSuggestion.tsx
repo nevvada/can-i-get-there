c9241f26f87cc9b9e7bb14823495e3ebd1671f7b
nlemast <nlemaster47@gmail.com>
2020-02-24 21:56:01 -0500

    Create helper function to render SubwayLogo component

--- src/FormSuggestion.tsx
+++ src/FormSuggestion.tsx
@@ -6,10 +6,27 @@ interface Props {
   stationInfo: Station;
 }
 
+const renderSubwayLineLogos = (subwayLines: Station['subwayLines']) => {
+  return subwayLines.map(subwayLine => {
+    return (
+      <SubwayLogo
+        colorClassName={subwayDict[subwayLine]}
+        key={subwayLine}
+        subwayLine={subwayLine}
+      />
+    )
+  });
+};
+
 const FormSuggestion = ({ stationInfo }: Props) => {
+  const { stationName, subwayLines } = stationInfo;
+
   return (
-    <h2>{stationInfo.stationName}</h2>
-  )
+    <>
+      <h2>{stationName}</h2>
+      {renderSubwayLineLogos(subwayLines)}
+    </>
+  );
 };
 
 export default FormSuggestion;
